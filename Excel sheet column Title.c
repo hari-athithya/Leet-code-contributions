44a53360290ad4b58acb9883b93ec04c6f339186
Hari_Athithya <hariathithya_bcs27@mepcoeng.ac.in>
2026-03-06 20:34:29 +0530

    Delete Excel sheet column Title.c

--- Excel sheet column Title.c
+++
@@ -1,7 +0,0 @@
-int titleToNumber(char* columnTitle) {
-    int result = 0;
-    for (int i = 0; columnTitle[i] != '\0'; i++) {
-        result = result * 26 + (columnTitle[i] - 64);
-    }
-    return result;
-}
